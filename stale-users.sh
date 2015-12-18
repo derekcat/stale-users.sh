57e4c8cce8120e921492ac703b955f4a65b99501
Derekcat <derekcat@gmail.com>
2015-12-18 13:52:49 -0800

    Update stale-users.sh
    Change behavior such that -p only prints stale users, added -a to print all users' login ages,

--- stale-users.sh
+++ stale-users.sh
@@ -13,8 +13,10 @@ if [ "$1" = "-h" ] || [ "$1" = "--help" ] || [ "$1" = "" ]; then
 	echo "Options"
 	echo "-h, --help			Display this help message"
 	echo "-e [email-to address], --email	Email the results to [email-to address]"
-	echo "-p, --print			Print to STDOUT instead of emailing the list"
-	echo "-u [username], --user		Same as -p, but only for the specified user"
+	echo "-p, --print			Print stale users to STDOUT instead of emailing the list"
+	echo "-a, --all			Print all users' login ages, stale or not"
+	echo "-u [username], --user		Print login age only for the specified user"
+	echo "				If no user specified, defaults to -p behavior"
 	exit
 fi
 
@@ -39,24 +41,26 @@ if [ $1 = "-p" ] || [ $1 = "--print" ] || [ $1 = "-u" ] || [ $1 = "--user" ]; th
 		
 		# When was their last login time?
 		LASTLOGINTIME="$(pwpolicy -u $USER --get-effective-policy | awk '{print $6}' | cut -d = -f 2)"
-
-		echo "Unixtime is $UNIXTIME" 
-		echo "Last login time is $LASTLOGINTIME" 
 		
 		# Age of the login in seconds
 		AGE=$((UNIXTIME - LASTLOGINTIME))
-		echo "$USER's last login was about $((AGE / 86400)) days ago." 
+
+		# If we're doing the big list, the UNIX times are going to make the list huge-R
+		# So only print these for a specific user
+		if [ $1 = "-u" ] || [ $1 = "--user" ]; then 
+			echo "Unixtime is $UNIXTIME" 
+			echo "Last login time is $LASTLOGINTIME" 
+			echo "$USER's last login was about $((AGE / 86400)) days ago."
+		fi
 	
 		# If they're over 180 days, add them to the list
 		if (( $AGE >= 15552000 )); then 
 			STALEUSERS+="$USER "
+			echo "$USER's last login was about $((AGE / 86400)) days ago." 
 		fi
 	done
 	
-	if [[ -n "$STALEUSERS" ]]; then # If $STALEUSERS is not empty, print them
-		echo "Stale user[s]:$STALEUSERS"
-		exit
-	else
+	if [[ -z "$STALEUSERS" ]]; then # If $STALEUSERS is empty, let us know
 		echo "No one is stale, yay!"
 		exit
 	fi
@@ -81,10 +85,40 @@ if [ $1 = "-e" ] || [ $1 = "--email" ]; then # If we're emailing the list
 	done
 	
 	if [[ -n "$STALEUSERS" ]]; then # If $STALEUSERS is not empty, email the list
-		echo "Stale user[s]:$STALEUSERS" | mail -s "Subject: server has stale users" "$EMAILADDRESS"
+		echo "Stale user[s]:$STALEUSERS" | mail -s "Subject: Pan has stale users" "$EMAILADDRESS"
 		exit
 	else
 		echo "No one is stale, yay!  Let's not bother sending an email."
+		exit
+	fi
+fi
+
+# If we're printing everyone's age to STDOUT
+if [ $1 = "-a" ] || [ $1 = "--all" ]; then 
+	echo "Now checking for stale users, please wait..."
+
+	for USER in $ODUSERS # Step through our list of users
+	do
+		
+		# When was their last login time?
+		LASTLOGINTIME="$(pwpolicy -u $USER --get-effective-policy | awk '{print $6}' | cut -d = -f 2)"
+		
+		# Age of the login in seconds
+		AGE=$((UNIXTIME - LASTLOGINTIME))
+		echo "$USER's last login was about $((AGE / 86400)) days ago." 
+	
+		# If they're over 180 days, add them to the list
+		if (( $AGE >= 15552000 )); then 
+			STALEUSERS+="$USER "
+		fi
+	done
+	
+	if [[ -n "$STALEUSERS" ]]; then # If $STALEUSERS is not empty, print them
+		echo "Stale user[s]:$STALEUSERS"
+		echo "You may wish to rerun with -p to see only the stale user's last login age"
+		exit
+	else
+		echo "No one is stale, yay!"
 		exit
 	fi
 fi
